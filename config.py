e035566f7da339e5a45154436f7f5a8ffa026b47
Harmya Surani <93425275+HarmyaSurani@users.noreply.github.com>
2023-04-13 15:27:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,16 +19,16 @@ def is_enabled(value, default):
 API_ID = int(os.environ.get("API_ID", "5605321"))
 # API Hash from https://my.telegram.org/auth
 API_HASH = os.environ.get("API_HASH", "178cd2fc5f7c4517bc783ac5a5362a1c")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6038977580:AAEjjWf5Qs5AOcs21He1IFsSyY4ks0vZvWY")  # Bot token from @BotFather
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5969083732:AAHrbP5jIWEnn86fR5bVEo6fHcbSK3ZP5jw")  # Bot token from @BotFather
 ADMINS = (
     [int(i.strip()) for i in os.environ.get("ADMINS").split(",")]
     if os.environ.get("ADMINS")
     else []
 )
 
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "linkshortify")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "bot")
 DATABASE_URL = os.environ.get(
-    "DATABASE_URL", "mongodb+srv://linkshortifycom:GUN07gun07@linkshortify.qkarnlr.mongodb.net/?retryWrites=true&w=majority"
+    "DATABASE_URL", "mongodb+srv://bot:GUN07gun07@bot.pqkvdtt.mongodb.net/?retryWrites=true&w=majority"
 )  # mongodb uri from https://www.mongodb.com/
 OWNER_ID = int(os.environ.get("OWNER_ID", "1446498316"))  # id of the owner
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
